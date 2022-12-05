53877d314000194fb0e75d9ebefb8f6267c24ec3
Steveen120 <86340149+Steveen120@users.noreply.github.com>
2022-12-04 20:43:00 -0500

    Add files via upload

---
+++ Deber_cursos_calificaciones/src/main.ts
@@ -0,0 +1,107 @@
+import { Activity } from "./entities/activity";
+import { GradeBookSetup } from "./entities/gradeBookSeutp";
+import { Student } from "./entities/student";
+import { Teacher } from "./entities/teacher";
+
+let students: Student[] = [];
+let teachers: Teacher[] = [];
+let activities:Activity[] = [];
+let gradesBookSetup: GradeBookSetup[] = [];
+
+
+enum Course{
+    Programacion = "Programacion Visual",
+    BaseDatos = "Base de Datos avanzada",
+    Metodologia = "Metodologia de Desarrollo de Software",
+}
+
+enum Area{
+    Desarrollo = "Desarrollo de Software",
+    Bomberos = "Bomberos",
+    ArteCulinario = "Arte Culinario",
+}
+
+function addStudent(): void {
+    let currentStudent: Student = {
+        fullName: readFormHtml("fullName"),
+        identification: parseInt(readFormHtml("identification")),
+        mail: readFormHtml("mail"),
+        direction: readFormHtml("direction"),
+        enrollment: parseInt(readFormHtml("enrollment")),
+        level: readFormHtml("level"),
+    }
+    students.push(currentStudent);
+    console.table(students);
+}
+
+function addTeacher(): void {
+    let currentTeacher: Teacher = {
+        fullName: readFormHtml("fullName-teacher"),
+        identification: parseInt(readFormHtml("identification-teacher")),
+        mail: readFormHtml("mail-teacher"),
+        direction: readFormHtml("direction-teacher"),
+        title : readFormHtml("title-teacher"),
+        area : readFormHtml("area-teacher")
+    }
+    teachers.push(currentTeacher);
+    //console.log(teachers);
+    console.table(teachers);
+}
+function addActivity(): void {
+    let currentActivity: Activity = {
+        name: readFormHtml("name-activity"),
+    }
+    activities.push(currentActivity);
+    console.table(activities);
+    initCourse();
+}
+
+function addGradeBookSetup(): void {
+    let currentGradeBookSetup: GradeBookSetup = {
+        value: readFormHtml("value-gradebook"),
+        course: readFormHtml("course-gradebook"),
+        activity: readFormHtml("activity-gradebook"),
+        maximunGrade: parseInt(readFormHtml("maximungrade-gradebook")),
+    }
+    gradesBookSetup.push(currentGradeBookSetup);
+    console.table(gradesBookSetup);
+}
+
+function initCourse():void{
+    let courseGradeBook = document.getElementById("course-gradebook") as HTMLSelectElement;
+    let courses = Object.values(Course);
+courses.forEach(
+    (value) => {
+     let option = document.createElement("option"); 
+       option.value = value;
+       option.text = value,
+       courseGradeBook.add(option);
+    }
+);
+}
+initCourse();
+function initArea():void{
+    let area = document.getElementById("area-teacher") as HTMLSelectElement;
+    let areas = Object.values(Area);
+areas.forEach(
+    (value) => {
+     let option = document.createElement("option"); 
+       option.value = value;
+       option.text = value,
+       area.add(option);
+    }
+);
+}
+initArea();
+
+
+
+
+
+
+
+
+
+function readFormHtml(id: string): string {
+    return (<HTMLInputElement>document.getElementById(id))!.value;
+}
